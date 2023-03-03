e15a3077b3d81d192132a49022d84994fbcda07b
amalagoli79 <adriano.g.malagoli@gmail.com>
2023-03-03 20:33:33 -0300

    UC12_Cod-Back-End - ER06 - Turma16 - v1

---
+++ T16_Cod_Back-End/ER-02/T16CodBackEnd/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using T16CodBackEnd.Classes;
+
+// metodo construtor
+PessoaFisica novaPf = new PessoaFisica();
+
+novaPf.nome = "Adriano";
+novaPf.cpf = 256854745;
+novaPf.dataNasc = new DateTime(2001, 01, 01);
+novaPf.endereco = "Rua Niteroi, 180";
+novaPf.rendimento = 12000.00f; 
+
+Console.WriteLine(novaPf.nome);
+Console.WriteLine(novaPf.cpf);
+Console.WriteLine(novaPf.dataNasc);
+Console.WriteLine(novaPf.endereco);
+Console.WriteLine(novaPf.rendimento);
+
+
+// Concatenação
+Console.WriteLine("Nome do Funcionário: " + novaPf.nome);
+
+// Interpolação
+Console.WriteLine($"Nome do Funcionário: {novaPf.nome} - CPF: {novaPf.cpf}");
+
+
---
+++ T16_Cod_Back-End/ER-03/T16CodBackEnd/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using T16CodBackEnd.Classes;
+
+// metodo construtor
+//PessoaFisica novaPf = new PessoaFisica();
+
+
+// novaPf.nome = "Adriano";
+// novaPf.cpf = 256854745;
+// novaPf.dataNasc = new DateTime(2001, 01, 01);
+// novaPf.endereco = "Rua Niteroi, 180";
+// novaPf.rendimento = 10000.00f; 
+
+// Console.WriteLine(novaPf.nome);
+// Console.WriteLine(novaPf.cpf);
+// Console.WriteLine(novaPf.dataNasc);
+// Console.WriteLine(novaPf.endereco);
+// Console.WriteLine(novaPf.rendimento);
+// // Concatenação
+// Console.WriteLine("Nome do Funcionário: " + novaPf.nome);
+// // Interpolação
+// Console.WriteLine($"Nome do Funcionário: {novaPf.nome} - CPF: {novaPf.cpf}");
+
+// Console.WriteLine(novaPf.CalcularImposto(novaPf.rendimento));
+
+PessoaJuridica novaPj = new PessoaJuridica();
+//float x = novaPj.rendimento = 16600.3f;
+//Console.WriteLine(novaPj.CalcularImposto(x));
+
+float impostoPagar = novaPj.CalcularImposto(10000.5f);
+Console.WriteLine($"O imposto a pagar será de : {impostoPagar:0.00}");
+Console.WriteLine(impostoPagar.ToString("C"));
+
+
+
+
